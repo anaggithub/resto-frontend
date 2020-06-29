6d6474d9241710ed052ecf1cb1eb50e10af927d8
Anahi <anahigaetan@hotmail.com>
2020-06-29 17:12:45 -0300

    Login Form

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DefaultLayout from "../../components/layout";
+import Input from "../../components/forms/input";
+import Button from "../../components/forms/button";
 
 const Login = () => {
-  // const dispatch = useDispatch();
 
-  // function handleOnClick(pet) {
-  //   dispatch(setPet(pet));
-  //   history.push("/chat");
-  // }
+  return (
 
-  return <div>LOGIN</div>;
+    <DefaultLayout>
+      <form>
+        <Input><label>Usuario o Correo Electrónico</label></Input>
+        <Input><label>Contraseña</label></Input>
+        <Button >Registrarse</Button>
+        <Button >Iniciar Sesión</Button>
+      </form>
+
+    </DefaultLayout>
+  );
 };
 
 export default Login;
