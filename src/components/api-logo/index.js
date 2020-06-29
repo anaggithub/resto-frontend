a7fa134572a34ff997654702ddb8f5f9d5772888
Anahi <anahigaetan@hotmail.com>
2020-06-29 15:42:30 -0300

    Fixed Name of Api-Logo to pascalCase

--- src/components/api-logo/index.js
+++ src/components/api-logo/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import "./index.scss";
 import styled from 'styled-components'
 
-const APILogo = styled.p`
+const ApiLogo = styled.p`
 font-family: "Bree Serif", serif;
 font-size:24px;
 font-weight:300;
@@ -10,7 +9,7 @@ font-weight:300;
 
 function APILogo() {
     return (
-        <APILogo>Delilah Resto</APILogo>
+        <ApiLogo>Delilah Resto</ApiLogo>
     );
 }
 
