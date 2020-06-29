12c495f2174b649e27d913b6de638267d6c8cbb6
Anahi <anahigaetan@hotmail.com>
2020-06-29 18:28:04 -0300

    Styled comp fix

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,25 +3,25 @@ import Header from "../header";
 import Footer from "../footer";
 import styled from 'styled-components';
 
+const StyledDefaultLayout = styled.div`
+background-color: pink;
+position: relative;
+display: flex;
+flex-direction: column;
+width: 100%;
+min-height: 100vh;
+`;
+
 const DefaultLayout = ({ children }) => {
 
-    const DefaultLayout = styled.div`
-    background-color: pink;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    min-height: 100vh;
-    `;
-
     return (
-        <DefaultLayout >
+        <StyledDefaultLayout >
             <Header />
             <div >
                 {children}
             </div>
             <Footer />
-        </DefaultLayout>
+        </StyledDefaultLayout>
     );
 }
 
