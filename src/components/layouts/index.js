112ccbfaa055b70d7028ed0fa8bb61d812e9df45
Anahi <anahigaetan@hotmail.com>
2020-06-29 15:43:38 -0300

    Deleting unnecessary files

--- src/components/layouts/index.js
+++
@@ -1,6 +0,0 @@
-import DefaultLayout from "./layout-default";
-import LayoutWithSearch from "./layout-search";
-import LayoutArtists from "./layout-artistspage";
-
-export default DefaultLayout;
-export {LayoutWithSearch, LayoutArtists}
