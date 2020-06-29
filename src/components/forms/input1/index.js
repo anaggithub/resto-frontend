041a2de79f279264cf58e09f21a5ec3772e3e6c5
Anahi <anahigaetan@hotmail.com>
2020-06-29 17:12:11 -0300

    Login and Form components

--- src/components/forms/input1/index.js
+++ src/components/forms/input1/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-//import "./index.scss";
+import React, { Children } from "react";
+import "./index.scss";
 
 function Input({ name, type, placeholder, onChange, error, errorMessage, classes, children }) {
     return (
         <div className={`input ${classes}`}>
-            <div className="input--row input--row--main">
+            <div className={`input--row ${error ? 'errorClass' : ""}`}>
                 <div className="input--row--chilchen">{children}</div>
                 <input
-                    className={`input--row--input ${error ? 'errorClass' : ""}`}
+                    className="input--row--input"
                     name={name}
                     type={type}
                     placeholder={placeholder}
