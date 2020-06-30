3b3236fdbfe8610442f5e3912643faf49d196757
Anahi <anahigaetan@hotmail.com>
2020-06-30 15:59:53 -0300

    Adding styles with styled comp

--- src/components/forms/button/index.js
+++ src/components/forms/button/index.js
@@ -1,13 +1,46 @@
 import React from "react";
-//import "./index.scss";
+import styled, { css } from 'styled-components';
 
-function Button({ classes, onClick, children }) {
+const StyledButton = styled.button`
+text-decoration: none;
+cursor: pointer;
+border-radius: 5px;
+padding: 0.5rem 0.7rem;
+transition: opacity 0.6s ease-in-out;
+font-size: 12px;
+font-weight: 700;
+border: solid 1.3px #FF6347;
+border: solid 1.3px palevioletred;
+&:hover {
+  opacity: 0.6;
+}
+${props => props.addCSS}
+${({ variant }) => variants[variant]}
+`;
+
+const variants = {
+    primary: css`
+    background-color:#FF6347;
+    background-color:palevioletred;
+    color:white;
+  `,
+    secondary: css`
+    background-color:white;
+    color:#FF6347;
+    color:palevioletred;
+  `,
+    special: css`
+    border:none;
+    color: palevioletred;  `
+}
+function Button({ classes, onClick, children, variant }) {
     return (
-        <div>
-            <button className={`button ${classes}`} onClick={onClick}>
-                {children}
-            </button>
-        </div>
+        <StyledButton
+            addCSS={classes}
+            variant={variant}
+            onClick={onClick} >
+            {children}
+        </StyledButton >
     );
 }
 
--- src/components/forms/input/index.js
+++ src/components/forms/input/index.js
@@ -1,25 +1,68 @@
 import React from "react";
-//import "./index.scss";
+import styled, { css } from 'styled-components';
 
-function Input({ name, type, placeholder, onChange, error, errorMessage, classes, children }) {
+const InputContainer = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+${props => props.addCSS}
+`
+const Row = styled.div`
+padding: 5px 15px;
+display: flex;
+flex-direction:column;
+width: 100%;
+`
+const ErrorRow = styled.div`
+display: flex;
+width: 100%;
+height: 40px;
+padding: 5px 15px;
+`
+const Label = styled.label`
+width: 100%;
+margin-bottom:10px;
+padding-left:5px;
+`
+const Input = styled.input`
+width: 100%;
+outline: none;
+border: 1px solid #c9c9c9;
+border-radius: 3px;
+font-size: 15px;
+padding: 10px;
+background: papayawhip;
+::placeholder {
+  color: palevioletred;
+}
+${props => props.addCSS}
+`
+const ErrorMessage = styled.p`
+padding-left:5px;
+`
+const errorClass = css`
+border: solid 1.5px #d80026;
+`;
+
+function InputBox({ name, type, placeholder, onChange, error, errorMessage, classes, labelText }) {
     return (
-        <div className={`input ${classes}`}>
-            <div className="input--row input--row--main">
-                <div className="input--row--chilchen">{children}</div>
-                <input
-                    className={`input--row--input ${error ? 'errorClass' : ""}`}
+        <InputContainer addCSS={classes}>
+            <Row>
+                <Label>{labelText}</Label>
+                <Input
+                    addCSS={error ? errorClass : ""}
                     name={name}
                     type={type}
                     placeholder={placeholder}
                     onChange={onChange}
                 >
-                </input>
-            </div>
-            <div className="input--row">
-                {error && <p className="input--row--error">{`${errorMessage}`}</p>}
-            </div>
-        </div>
+                </Input>
+            </Row>
+            <ErrorRow>
+                {error && <ErrorMessage>{`${errorMessage}`}</ErrorMessage>}
+            </ErrorRow>
+        </InputContainer>
     );
 }
 
-export default Input
+export default InputBox
