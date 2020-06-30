ec01d1bae2788d508cb16a21f215207511be418e
Anahi <anahigaetan@hotmail.com>
2020-06-30 16:00:38 -0300

    New styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import APILogo from "../api-logo";
 import { Link } from "react-router-dom";
 import styled from 'styled-components'
+
 const StyledHeader = styled.header`
-  background-color: blue;
     width: 100%;
     height: 80px;
     display: flex;
     justify-content: center;
     align-items: center;
     border-bottom: 1px solid #c9c9c9;
+    background-color:palevioletred;
     position: relative;
+    color:white;
   `;
 
 const Header = () => {
