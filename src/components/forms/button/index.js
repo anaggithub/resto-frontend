3288a96c72a86b48f464b7127ba5c9f8076b145c
Anahi <anahigaetan@hotmail.com>
2020-07-02 00:13:16 -0300

    Estilos de botones como el prototipo

--- src/components/forms/button/index.js
+++ src/components/forms/button/index.js
@@ -4,14 +4,14 @@ import styled, { css } from 'styled-components';
 const StyledButton = styled.button`
 text-decoration: none;
 cursor: pointer;
-border-radius: 5px;
-padding: 0.5rem 0.7rem;
+border-radius: 25px;
+padding: 0.7rem 2.4rem;
 transition: opacity 0.6s ease-in-out;
-font-size: 12px;
-font-weight: 700;
 color: #FF8C69;
 background-color:white;
 border: solid 1.3px #FF8C69;
+margin: 0.2rem;
+font-size:12px;
 &:hover {
   opacity: 0.6;
 }
@@ -28,9 +28,12 @@ const variants = {
     /* background-color:white; */
   `,
   special: css`
+  font-size:11px;
     border:none;
      `
 }
+
+
 function Button({ classes, onClick, children, variant }) {
   return (
     <StyledButton
