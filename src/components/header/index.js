bff046636bd31919724fb3df6a7a6b16e3955c2e
Anahi <anahigaetan@hotmail.com>
2020-07-02 00:13:32 -0300

    Estilos como el prototipo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,12 +5,13 @@ import styled from 'styled-components'
 
 const StyledHeader = styled.header`
     width: 100%;
-    height: 100px;
+    height: 120px;
     display: flex;
     justify-content: center;
     align-items: center;
     /* border-bottom: 1px solid #c9c9c9; */
     background-color: #AFEEEE;
+    background-color: #5fe4e4;
     /* //background-color:palevioletred; */
     color:white;
   `;
