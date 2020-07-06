d22efb429cc4602562887c6e3a15501d64c40329
Anahi <anahigaetan@hotmail.com>
2020-07-06 16:01:01 -0300

    Login helpers

---
+++ src/helpers/login.js
@@ -0,0 +1,12 @@
+const TOKEN_KEY = "RESTO_TOKEN";
+
+export function setToken(token) {
+    localStorage.setItem(TOKEN_KEY, token);
+}
+export function getToken() {
+    return localStorage.getItem(TOKEN_KEY);
+}
+
+export function deleteToken() {
+    localStorage.removeItem(TOKEN_KEY);
+}
