b3fe33ae8f0ca55d7ca679ff2706e899a9af919b
Anahi <anahigaetan@hotmail.com>
2020-07-07 23:20:39 -0300

    Login loading action added

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import { LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import { LOGIN_SUCCESS, LOGIN_FAILURE, LOGIN_LOADING } from './types';
 
 export const login = () => {
-
+    console.log("lala")
     return (dispatch) => {
-        console.log("lala")
         let url = "http://restohaditaheroku.herokuapp.com/login"
         let headers = {
             headers: {
@@ -12,10 +11,13 @@ export const login = () => {
                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             }
         };
+        dispatch({ type: LOGIN_LOADING });
+
         axios
             .post(url, {
                 username: "admin",
                 password: "Ad1023MinN"
+
             }, headers)
             .then(res => {
                 dispatch({
@@ -23,113 +25,44 @@ export const login = () => {
                     payload: res
                 });
             })
-
             .catch(e => {
                 dispatch({
-                    type: LOGIN_FAIL
+                    type: LOGIN_FAILURE
                 });
             })
     }
 }
-    // Headers
 
-    // axios
-    //     .post('/api/auth/login', body, config)
-    //     .then(res =>
-    //         dispatch({
-    //             type: LOGIN_SUCCESS,
-    //             payload: res.data
-    //         })
-    //     )
-    //     .catch(err => {
-    //         dispatch(
-    //             returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL')
-    //         );
-    //         dispatch({
-    //             type: LOGIN_FAIL
-    //         });
-    //     });
+// username: "admin",
+// password: "Ad1023MinN"
 
+//acá lo hice con await async
+// export const login = () => {
 
-
-// // Logout User
-// export const logout = () => {
-//     return {
-//         type: LOGOUT_SUCCESS
-//     };
-// };
-
-// // Setup config/headers and token
-// export const tokenConfig = (getState: Function) => {
-//     // Get token from localstorage
-//     const token = getState().auth.token;
-
-//     // Headers
-//     const config: IConfigHeaders = {
-//         headers: {
-//             'Content-type': 'application/json'
+//     return async (dispatch) => {
+//         console.log("lala")
+//         let url = "http://restohaditaheroku.herokuapp.com/login"
+//         let headers = {
+//             headers: {
+//                 'Access-Control-Allow-Origin': '*',
+//                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//             }
+//         };
+//         let res = await axios
+//             .post(url, {
+//                 username: "pablog",
+//                 password: "pedrog1986"
+//             }, headers)
+//         if (res.status === 200) {
+//             dispatch({
+//                 type: LOGIN_SUCCESS,
+//                 payload: res
+//             });
 //         }
-//     };
-
-//     // If token, add to headers
-//     if (token) {
-//         config.headers['x-auth-token'] = token;
+//         else {
+//             dispatch({
+//                 type: LOGIN_FAIL
+//             });
+//         }
 //     }
-
-//     return config;
-// };
-
-
-// // Check token & load user
-// export const loadUser = () => (dispatch: Function, getState: Function) => {
-//     // User loading
-//     dispatch({ type: USER_LOADING });
-
-//     axios
-//         .get('/api/auth/user', tokenConfig(getState))
-//         .then(res =>
-//             dispatch({
-//                 type: USER_LOADED,
-//                 payload: res.data
-//             })
-//         )
-//         .catch(err => {
-//             dispatch(returnErrors(err.response.data, err.response.status));
-//             dispatch({
-//                 type: AUTH_ERROR
-//             });
-//         });
-// };
-
-// // Register User
-// export const register = ({ name, email, password }: IAuthFunction) => (
-//     dispatch: Function
-// ) => {
-//     // Headers
-//     const config = {
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     };
-
-//     // Request body
-//     const body = JSON.stringify({ name, email, password });
-
-//     axios
-//         .post('/api/auth/register', body, config)
-//         .then(res =>
-//             dispatch({
-//                 type: REGISTER_SUCCESS,
-//                 payload: res.data
-//             })
-//         )
-//         .catch(err => {
-//             dispatch(
-//                 returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL')
-//             );
-//             dispatch({
-//                 type: REGISTER_FAIL
-//             });
-//         });
-// };
-
+// }
