157f629319e3395c54597661e9f9d8e65428ad4e
Anahi <anahigaetan@hotmail.com>
2020-07-07 23:22:40 -0300

    Names of action types updated

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,10 +1,6 @@
-// export const USER_LOADING = "USER_LOADING";
-// export const USER_LOADED = "USER_LOADED";
-// export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAIL = "LOGIN_FAIL";
-// export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
-// export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
-// export const REGISTER_FAIL = "REGISTER_FAIL";
-// export const GET_ERRORS = 'GET_ERRORS';
-// export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const LOGIN_LOADING = "LOGIN_LOADING";
+export const ITEMS_GET_SUCCESS = "ITEMS_GET_SUCCESS";
+export const ITEMS_GET_FAILURE = "ITEMS_GET_FAILURE";
+export const ITEMS_GET_LOADING = "ITEMS_GET_LOADING";
