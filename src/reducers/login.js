225733b2e62d740043c6b6204a92eb136cd7c6d4
Anahi <anahigaetan@hotmail.com>
2020-07-07 23:21:30 -0300

    Login loading action added

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,30 +1,37 @@
-import { LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/types';
+import { LOGIN_SUCCESS, LOGIN_LOADING, LOGIN_FAILURE } from '../actions/types';
 import { setToken, getToken, deleteToken } from "../helpers/login"
 
 const initialState = {
     token: getToken() || "",
     isAuthenticated: null,
-    isAdmin: false
+    isAdmin: null,
+    isLoading: false
 };
 
 const login = (state = initialState, action) => {
     switch (action.type) {
+        case LOGIN_LOADING:
+            return {
+                ...state,
+                isLoading: true
+            };
         case LOGIN_SUCCESS:
             setToken(action.payload.data.token)
-            console.log(action.payload)
             return {
                 ...state,
                 token: action.payload.data.token,
                 isAuthenticated: true,
-                isAdmin: action.payload.data.role === 1 ? true : false
+                isAdmin: action.payload.data.role === 1 ? true : false,
+                isLoading: false
             };
-        case LOGIN_FAIL:
+        case LOGIN_FAILURE:
             deleteToken()
             return {
                 ...state,
                 token: "",
-                isAdmin: false,
                 isAuthenticated: false,
+                isAdmin: false,
+                isLoading: false
             };
         default:
             return state;
