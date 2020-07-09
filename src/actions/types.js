6583d089798fb0ef046577ae1f8d4ef582bab74b
Anahi <anahigaetan@hotmail.com>
2020-07-09 00:52:55 -0300

    Action names fix

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,6 @@
+export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
-export const LOGIN_LOADING = "LOGIN_LOADING";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const ITEMS_GET_START = "ITEMS_GET_START";
 export const ITEMS_GET_SUCCESS = "ITEMS_GET_SUCCESS";
-export const ITEMS_GET_FAILURE = "ITEMS_GET_FAILURE";
-export const ITEMS_GET_LOADING = "ITEMS_GET_LOADING";
+export const ITEMS_GET_FAIL = "ITEMS_GET_FAIl";
