c8a5ad77196abddd68d9fa7f2dbf105078edde37
Anahi <anahigaetan@hotmail.com>
2020-07-09 00:56:09 -0300

    Items reducer

---
+++ src/reducers/items.js
@@ -0,0 +1,33 @@
+import { ITEMS_GET_SUCCESS, ITEMS_GET_FAIL, ITEMS_GET_START } from './types';
+
+const initialState = {
+    items: [],
+    isLoading: false
+};
+
+const items = (state = initialState, action) => {
+    console.log(action.payload.data)
+    switch (action.type) {
+        case ITEMS_GET_START:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case ITEMS_GET_SUCCESS:
+            return {
+                ...state,
+                items: action.payload.data,
+                isLoading: false
+            }
+        case ITEMS_GET_FAIL:
+            return {
+                ...state,
+                items: [],
+                isLoading: false
+            }
+        default:
+            return state;
+    }
+}
+
+export default items
