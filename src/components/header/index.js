f37962ed084c63d19aad49bb83147f04d424123d
Anahi <anahigaetan@hotmail.com>
2020-07-09 00:54:23 -0300

    Styles moved into styles.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,20 +1,7 @@
 import React from "react";
 import APILogo from "../api-logo";
 import { Link } from "react-router-dom";
-import styled from 'styled-components'
-
-const StyledHeader = styled.header`
-    width: 100%;
-    height: 120px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    /* border-bottom: 1px solid #c9c9c9; */
-    background-color: #AFEEEE;
-    background-color: #5fe4e4;
-    /* //background-color:palevioletred; */
-    color:white;
-  `;
+import { StyledHeader } from "./styles"
 
 const Header = ({ classes }) => {
 
