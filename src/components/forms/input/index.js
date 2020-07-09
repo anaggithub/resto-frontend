9cae0d15c74b131e5c471d7efda3f7161e2adcb8
Anahi <anahigaetan@hotmail.com>
2020-07-09 00:54:36 -0300

    Styles moved into styles.js

--- src/components/forms/input/index.js
+++ src/components/forms/input/index.js
@@ -1,49 +1,5 @@
 import React from "react";
-import styled, { css } from 'styled-components';
-
-const InputContainer = styled.div`
-display: flex;
-flex-direction: column;
-width: 100%;
-${props => props.addCSS}
-`
-const Row = styled.div`
-padding: 5px 15px;
-display: flex;
-flex-direction:column;
-width: 100%;
-`
-const ErrorRow = styled.div`
-display: flex;
-width: 100%;
-height: 40px;
-padding: 5px 15px;
-`
-const Label = styled.label`
-width: 100%;
-margin-bottom:10px;
-padding-left:5px;
-font-size:0.8rem;
-`
-const StyledInput = styled.input`
-width: 100%;
-outline: none;
-border: 1px solid #c9c9c9;
-border-radius: 3px;
-font-size: 15px;
-padding: 10px;
-/* background: papayawhip; */
-::placeholder {
-  /* color: palevioletred; */
-}
-${props => props.addCSS}
-`
-const ErrorMessage = styled.p`
-padding-left:5px;
-`
-const errorClass = css`
-border: solid 1.5px #d80026;
-`;
+import { InputContainer, Row, Label, StyledInput, ErrorRow, ErrorMessage, errorClass } from "./styles"
 
 function Input({ name, type, placeholder, onChange, error, errorMessage, classes, labelText }) {
     return (
