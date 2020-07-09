2a3ebc4411ab22af9dee84c4ad857fcaa7c27e05
Anahi <anahigaetan@hotmail.com>
2020-07-09 00:54:54 -0300

    Styles moved into styles.js

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,19 +1,6 @@
 import React from "react";
-import styled from 'styled-components';
 import Header from "../header";
-
-const StyledDefaultLayout = styled.div`
-width: 100%;
-min-height: 100vh;
-display: flex;
-flex-direction:column;
-`;
-const Children = styled.div`
-width: 100%;
-flex-grow: 1;
-display: flex;
-flex-direction:column;
-`;
+import { StyledDefaultLayout, Children } from "./styles"
 
 const DefaultLayout = ({ children }) => {
     return (
