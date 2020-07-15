bd1e6f2bdd9802f7e65c643154acd7b771af1398
Anahi <anahigaetan@hotmail.com>
2020-07-15 00:04:27 -0300

    Items Box for Home

--- src/components/item-box/index.js
+++ src/components/item-box/index.js
@@ -1,12 +1,20 @@
 import React from "react"
+import Button from "../../components/forms/button";
+import { ItemsContainer, Item, ItemData, ItemImage } from "./styles"
 
-const ItemBox = ({ id, name, picture }) => {
+const ItemBox = ({ name, price, picture }) => {
 
     return (
-        <div>
-            <h3>{name}</h3>
-            <img src={picture} />
-        </div>
+        <ItemsContainer>
+            <Item>
+                <ItemImage src={picture} />
+                <ItemData>
+                    <h4>{name}</h4>
+                    <p>${price}</p>
+                </ItemData>
+            </Item>
+            <Button variant="add" ><i className="fas fa-plus"></i></Button>
+        </ItemsContainer>
     )
 }
 
---
+++ src/components/item-box/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const ItemsContainer = styled.div`
+width:100%;
+display:flex;
+margin: 20px 0;
+`;
+
+export const Item = styled.div`
+width:100%;
+display:flex;
+`;
+
+export const ItemData = styled.div`
+width:100%;
+display:flex;
+flex-direction:column;
+justify-content: center;
+padding-left:10px;
+h4{
+font-size:1rem;
+}
+p{
+font-size:0.8rem;
+}
+`;
+
+export const ItemImage = styled.img`
+width:70px;
+height:auto;
+
+`;
