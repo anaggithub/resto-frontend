74f36cae3e960ed3674699b5517f6c933472b346
Anahi <anahigaetan@hotmail.com>
2020-07-15 00:05:07 -0300

    Home Styles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import DefaultLayout from "../../components/layout";
 import ItemBox from "../../components/item-box";
+import { HomeContainer, Title } from "./styles"
 //import Spinner from "../../components/loading-spinner"
 import { useDispatch, useSelector } from "react-redux";
 import { getItems } from "../../actions/items";
@@ -16,18 +17,19 @@ const Home = () => {
 
   return (
     <DefaultLayout>
-      <section>
-        <h1>Home</h1>
+      <HomeContainer>
+        <Title>Nuestros platos</Title>
         <div>
           {products.length > 0 && products.map(
             (product) => (
               <ItemBox key={product.product_id + 1}
                 name={product.product}
+                price={product.price}
                 picture={product.picture} />
             )
           )}
         </div>
-      </section>
+      </HomeContainer>
     </DefaultLayout>
   );
 };
---
+++ src/pages/home/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+font-size:1rem;
+`;
+
+export const HomeContainer = styled.section`
+padding:10px;
+`;
