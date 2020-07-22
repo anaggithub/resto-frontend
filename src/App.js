63b3ed79043e367e16077ebea5512eb2216fec80
Anahi <anahigaetan@hotmail.com>
2020-07-22 17:13:30 -0300

    Adding Theme Provider to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 import store from "./store/index.js";
+import ThemeProvider from "./themes";
 import { Provider as StoreProvider } from "react-redux";
 import { deleteToken } from "./helpers/login"
 import {
@@ -16,16 +17,19 @@ import {
 const App = () => {
   const history = useHistory();
   deleteToken()
+
   return (
     <StoreProvider store={store}>
-      <Router>
-        <Switch history={history}>
-          <Route exact path="/" render={() => <Redirect to="/home" />} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/dashboard" component={Dashboard} />
-        </Switch>
-      </Router>
+      <ThemeProvider>
+        <Router>
+          <Switch history={history}>
+            <Route exact path="/" render={() => <Redirect to="/home" />} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </StoreProvider>
   );
 };
