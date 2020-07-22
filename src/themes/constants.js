31c1dc53fa355651a54d2043e310937a4d94d305
Anahi <anahigaetan@hotmail.com>
2020-07-22 17:11:23 -0300

    Creating themes constants

---
+++ src/themes/constants.js
@@ -0,0 +1,19 @@
+export const light = {
+    name: 'light',
+    colors: {
+        background: 'white',
+        primary: 'blue',
+        body: 'black',
+        white: "whitesmoke",
+        white: "whitesmoke"
+    },
+};
+
+export const dark = {
+    name: 'dark',
+    colors: {
+        background: 'black',
+        primary: 'pink',
+        body: 'white',
+    },
+};
