8fe09ef4b7ac8a8bfd06d895fd4d6e4e5cacc07f
Anahi <anahigaetan@hotmail.com>
2020-07-23 13:08:24 -0300

    Theme colors added to Header

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 
 export const StyledHeader = styled.header`
     width: 100%;
-    height: 120px;
+    height: 80px;
     display: flex;
     justify-content: center;
     align-items: center;
-    /* border-bottom: 1px solid #c9c9c9; */
-    background-color: #AFEEEE;
-    background-color: #5fe4e4;
-    /* //background-color:palevioletred; */
-    color:white;
+    /* border-bottom: 1px solid #c9c9c9; */   
+    background-color:${({ theme }) => theme.colors.lightBlue};;
+   p{
+    color: ${({ theme }) => theme.colors.white};
+   } 
   `;
