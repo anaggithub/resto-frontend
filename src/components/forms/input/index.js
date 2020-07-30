68424d904c8d8dc3d5ca4508b484832fdbdfb93f
anahi-gaetan <anahi.gaetan@globant.com>
2020-07-30 12:16:17 -0300

    Adding a controlled value to inputs

--- src/components/forms/input/index.js
+++ src/components/forms/input/index.js
@@ -1,55 +1,76 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { InputContainer, Row, Label, StyledInput, ErrorRow, ErrorMessage, errorClass } from "./styles"
+import {
+  InputContainer,
+  Row,
+  Label,
+  StyledInput,
+  ErrorRow,
+  ErrorMessage,
+  errorClass,
+} from "./styles";
 
-function Input({ name, type, placeholder, maxLength, minLength, autoComplete, onChange, error, errorMessage, styles, labelText }) {
-    return (
-        <InputContainer styles={styles}>
-            <Row>
-                <Label>{labelText}</Label>
-                <StyledInput
-                    styles={error ? errorClass : ""}
-                    name={name}
-                    type={type}
-                    placeholder={placeholder}
-                    onChange={onChange}
-                    maxLength={maxLength}
-                    minLength={minLength}
-                    autoComplete={autoComplete}
-                >
-                </StyledInput>
-            </Row>
-            <ErrorRow>
-                {error && <ErrorMessage>{`${errorMessage}`}</ErrorMessage>}
-            </ErrorRow>
-        </InputContainer>
-    );
+function Input({
+  labelText,
+  name,
+  value,
+  type,
+  placeholder,
+  maxLength,
+  minLength,
+  autoComplete,
+  onChange,
+  error,
+  errorMessage,
+  styles,
+}) {
+  return (
+    <InputContainer styles={styles}>
+      <Row>
+        <Label>{labelText}</Label>
+        <StyledInput
+          name={name}
+          value={value}
+          type={type}
+          placeholder={placeholder}
+          maxLength={maxLength}
+          minLength={minLength}
+          autoComplete={autoComplete}
+          onChange={onChange}
+          styles={error ? errorClass : ""}
+        ></StyledInput>
+      </Row>
+      <ErrorRow>
+        {error && <ErrorMessage>{`${errorMessage}`}</ErrorMessage>}
+      </ErrorRow>
+    </InputContainer>
+  );
 }
 
 Input.propTypes = {
-    name: PropTypes.string,
-    type: PropTypes.string,
-    placeholder: PropTypes.string,
-    maxLength: PropTypes.string,
-    minLength: PropTypes.string,
-    autoComplete: PropTypes.string,
-    labelText: PropTypes.string,
-    onChange: PropTypes.func,
-    error: PropTypes.bool,
-    errorMessage: PropTypes.string,
-    styles: PropTypes.object
+  name: PropTypes.string,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  maxLength: PropTypes.string,
+  minLength: PropTypes.string,
+  autoComplete: PropTypes.string,
+  labelText: PropTypes.string,
+  onChange: PropTypes.func,
+  error: PropTypes.bool,
+  errorMessage: PropTypes.string,
+  styles: PropTypes.object,
 };
 
 Input.defaultProps = {
-    name: "",
-    type: "",
-    placeholder: "",
-    maxLength: "",
-    minLength: "",
-    autoComplete: "",
-    labelText: "",
-    errorMessage: "",
-    styles: {}
+  name: "",
+  type: "",
+  placeholder: "",
+  maxLength: "",
+  minLength: "",
+  autoComplete: "",
+  labelText: "",
+  errorMessage: "",
+  styles: {},
 };
 
-export default Input
+export default Input;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from "react";
 import DefaultLayout from "../../components/layout";
 import Input from "../../components/forms/input";
 import Button from "../../components/forms/button";
-import Spinner from "../../components/loading-spinner"
-import { Form, InputsBox, ButtonsBox, SpinnerBox } from "./styles"
-import { useHistory } from 'react-router-dom'
+import Spinner from "../../components/loading-spinner";
+import { Form, InputsBox, ButtonsBox, SpinnerBox } from "./styles";
+import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../actions/login";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const history = useHistory()
+  const history = useHistory();
 
-  const [user, setUser] = useState("")
-  const [password, setPassword] = useState("")
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
   const [userError, setUserError] = useState(false);
   const [userErrorMessage, setUserErrorMessage] = useState("");
@@ -22,14 +22,14 @@ const Login = () => {
   const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
 
   const [isLoading, setLoading] = useState(false);
-  const loading = useSelector(state => state.login.isLoading);
-  const isAdmin = useSelector(state => state.login.isAdmin);
+  const loading = useSelector((state) => state.login.isLoading);
+  const isAdmin = useSelector((state) => state.login.isAdmin);
 
   function handleUserChange(e) {
-    setUser(e.target.value)
+    setUser(e.target.value);
   }
   function handlePasswordChange(e) {
-    setPassword(e.target.value)
+    setPassword(e.target.value);
   }
 
   function handleSubmit(e) {
@@ -41,29 +41,25 @@ const Login = () => {
     if (!user || user.trim() === "") {
       setUserErrorMessage("Ingrese un usuario");
       setUserError(true);
-    }
-    else if (!password || password.trim() === "") {
+    } else if (!password || password.trim() === "") {
       setPasswordErrorMessage("Ingrese una contraseña");
       setPasswordError(true);
-    }
-    else if (!user.match(letters)) {
+    } else if (!user.match(letters)) {
       setUserErrorMessage("Solo se aceptan letras.");
       setUserError(true);
-    }
-    else {
+    } else {
       dispatch(login(user, password));
     }
   }
 
   useEffect(() => {
     if (isAdmin === true) {
-      console.log("entro al admin true")
-      history.push('/dashboard');
-    }
-    else if (isAdmin === false) {
-      console.log(isAdmin)
-      console.log("entro al admin false")
-      history.push('/home');
+      console.log("entro al admin true");
+      history.push("/dashboard");
+    } else if (isAdmin === false) {
+      console.log(isAdmin);
+      console.log("entro al admin false");
+      history.push("/home");
     }
   }, [isAdmin]);
 
@@ -79,31 +75,29 @@ const Login = () => {
             name="user"
             type="text"
             labelText="Usuario o correo electrónico"
+            value={user}
             onChange={handleUserChange}
             error={userError}
             errorMessage={userErrorMessage}
             maxLength="30"
             autoComplete="on"
-          >
-          </Input>
-          <SpinnerBox>
-            {isLoading && <Spinner />}
-          </SpinnerBox>
+          ></Input>
+          <SpinnerBox>{isLoading && <Spinner />}</SpinnerBox>
           <Input
             name="password"
             type="password"
             labelText="Contraseña"
+            value={password}
             onChange={handlePasswordChange}
             error={passwordError}
             errorMessage={passwordErrorMessage}
             maxLength="10"
             autoComplete="on"
-          >
-          </Input>
+          ></Input>
         </InputsBox>
         <ButtonsBox>
-          <Button variant="primary" >INICIAR SESION</Button>
-          <Button variant="special" >REGISTRARSE</Button>
+          <Button variant="primary">INICIAR SESION</Button>
+          <Button variant="special">REGISTRARSE</Button>
         </ButtonsBox>
       </Form>
     </DefaultLayout>
